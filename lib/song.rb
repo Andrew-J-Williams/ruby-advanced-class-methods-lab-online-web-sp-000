b38c69cf6611049439e9e54f9a593c18c8ff413c
Andrew Williams <1williamsaj@gmail.com>
2019-09-26 02:05:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     if self.all.include?(name)
-      return self.find_by_name(name)
+      self.find_by_name(name)
     else
       self.create_by_name(name)
     end
